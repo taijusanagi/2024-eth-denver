fbd07cfd35e04b7d4b4b981687689a74cc00efd3
taijusanagi <taijusanagi@gmail.com>
2024-02-24 12:02:52 +0900

    ebable auto format for sol

--- contracts/contracts/Dungeon.sol
+++ contracts/contracts/Dungeon.sol
@@ -5,5 +5,28 @@ pragma solidity ^0.8.24;
 // import "hardhat/console.sol";
 
 contract Dungeon {
+    enum TileType {
+        Wall,
+        Plain
+    } // Enum to represent tile types
 
+    int public width;
+    uint public height;
+    uint public maxTunnels;
+    uint public maxLength;
+    TileType[][] public map; // 2D array for map
+    uint public maxDistance;
+    // Positions stored as two separate uints for simplicity
+    uint public startX;
+    uint public startY;
+    uint public endX;
+    uint public endY;
+
+    constructor(uint _width, uint _height, uint _maxTunnels, uint _maxLength) {
+        width = _width;
+        height = _height;
+        maxTunnels = _maxTunnels;
+        maxLength = _maxLength;
+        generateMap();
+    }
 }
