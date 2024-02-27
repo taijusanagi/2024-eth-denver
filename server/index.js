22e71964ac2651475c6d86e1e2bdcc7f883c6e2a
taijusanagi <taijusanagi@gmail.com>
2024-02-27 11:00:19 +0900

    working eth storage...

--- server/index.js
+++ server/index.js
@@ -7,242 +7,12 @@ const { ethers } = require("ethers");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const deployedSepoliaDirectory = "0x85c498a8a6a9ad9f0bdc13319e2276cd665d2de1";
-const deployedSepoliaNormalDirectory = "";
-const createdFileName = "1708988973302.txt";
-// const createdFileName = "1708990567024.txt";
+const deployedSepoliaBlobDirectory = "0x53E2e6379a5697f09C8Eedd4fE05Da4f9A977269";
+const deployedSepoliaNormalDirectory = "0x0eb32D2424B048120043A35489CC5913C2d50108";
+const createdBlobFileName = "1708998730273.txt";
+const createdNormalFileName = "1708998813573.txt";
 
 const IERC5018Abi = [
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-      {
-        internalType: "uint256",
-        name: "chunkId",
-        type: "uint256",
-      },
-    ],
-    name: "chunkSize",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-    ],
-    name: "countChunks",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "destruct",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-      {
-        internalType: "uint256",
-        name: "chunkId",
-        type: "uint256",
-      },
-    ],
-    name: "getChunkHash",
-    outputs: [
-      {
-        internalType: "bytes32",
-        name: "",
-        type: "bytes32",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-    ],
-    name: "read",
-    outputs: [
-      {
-        internalType: "bytes",
-        name: "",
-        type: "bytes",
-      },
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-      {
-        internalType: "uint256",
-        name: "chunkId",
-        type: "uint256",
-      },
-    ],
-    name: "readChunk",
-    outputs: [
-      {
-        internalType: "bytes",
-        name: "",
-        type: "bytes",
-      },
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "refund",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-    ],
-    name: "remove",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-      {
-        internalType: "uint256",
-        name: "chunkId",
-        type: "uint256",
-      },
-    ],
-    name: "removeChunk",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-    ],
-    name: "size",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-      {
-        internalType: "uint256",
-        name: "chunkId",
-        type: "uint256",
-      },
-    ],
-    name: "truncate",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
   {
     inputs: [
       {
@@ -268,110 +38,6 @@ const IERC5018Abi = [
         name: "name",
         type: "bytes",
       },
-      {
-        internalType: "uint256",
-        name: "chunkId",
-        type: "uint256",
-      },
-      {
-        internalType: "bytes",
-        name: "data",
-        type: "bytes",
-      },
-    ],
-    name: "writeChunk",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
-  },
-];
-const IERC5018ForBlobAbi = [
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-      {
-        internalType: "uint256",
-        name: "chunkId",
-        type: "uint256",
-      },
-    ],
-    name: "chunkSize",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-    ],
-    name: "countChunks",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "destruct",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-      {
-        internalType: "uint256",
-        name: "chunkId",
-        type: "uint256",
-      },
-    ],
-    name: "getChunkHash",
-    outputs: [
-      {
-        internalType: "bytes32",
-        name: "",
-        type: "bytes32",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
     ],
     name: "read",
     outputs: [
@@ -389,196 +55,12 @@ const IERC5018ForBlobAbi = [
     stateMutability: "view",
     type: "function",
   },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-      {
-        internalType: "uint256",
-        name: "chunkId",
-        type: "uint256",
-      },
-    ],
-    name: "readChunk",
-    outputs: [
-      {
-        internalType: "bytes",
-        name: "",
-        type: "bytes",
-      },
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "refund",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-    ],
-    name: "remove",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-      {
-        internalType: "uint256",
-        name: "chunkId",
-        type: "uint256",
-      },
-    ],
-    name: "removeChunk",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-    ],
-    name: "size",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-      {
-        internalType: "uint256",
-        name: "chunkId",
-        type: "uint256",
-      },
-    ],
-    name: "truncate",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "upfrontPayment",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "name",
-        type: "bytes",
-      },
-      {
-        internalType: "uint256[]",
-        name: "chunkIds",
-        type: "uint256[]",
-      },
-      {
-        internalType: "uint256[]",
-        name: "sizes",
-        type: "uint256[]",
-      },
-    ],
-    name: "writeChunk",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
-  },
-];
-const IFlatDirectoryAbi = [
-  {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "filename",
-        type: "bytes",
-      },
-    ],
-    name: "files",
-    outputs: [
-      {
-        internalType: "bytes",
-        name: "",
-        type: "bytes",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
 ];
 
 // this function only called for setup
 async function setupBlobDirectory() {
   const privateKey = process.env.PRIVATE_KEY;
-  const rpc = process.env.RPC;
+  const rpc = "https://gateway.tenderly.co/public/sepolia";
   const ethStorage = new EthStorage(rpc, privateKey);
   // this method uses below as blob manager
   // https://sepolia.etherscan.io/address/0x804C520d3c084C805E37A35E90057Ac32831F96f
@@ -586,27 +68,32 @@ async function setupBlobDirectory() {
   console.log(result);
 }
 
+// this function only called for setup
 async function setupNormalDirectory() {
   const privateKey = process.env.PRIVATE_KEY;
-  const rpc = process.env.RPC;
+  const rpc = "https://gateway.tenderly.co/public/sepolia";
   const ethStorage = new EthStorage(rpc, privateKey);
-  // this method uses below as blob manager
-  // https://sepolia.etherscan.io/address/0x804C520d3c084C805E37A35E90057Ac32831F96f
+  // this method uses empty address as blob manager
   const result = await ethStorage.deployNormalDirectory();
   console.log(result);
 }
 
-async function upload() {
+// success tx
+// 0xfcdc34a1a9be6a0fca22b23d0a901d74608f5df55757d9317f44e630ea0a504c
+// https://sepolia.etherscan.io/tx/0xfcdc34a1a9be6a0fca22b23d0a901d74608f5df55757d9317f44e630ea0a504c
+
+async function uploadToBlobDirectory() {
   const privateKey = process.env.PRIVATE_KEY;
   // should use rpc which suports blob upload
   const rpc = process.env.RPC;
-  const ethStorage = new EthStorage(rpc, privateKey, deployedSepoliaDirectory);
-  const content = "hello";
+  const ethStorage = new EthStorage(rpc, privateKey, deployedSepoliaBlobDirectory);
+  const content = "hello world!!     ";
 
   // Create a temporary file path
   const tempDir = os.tmpdir();
-  const tempFilePath = path.join(tempDir, `${Date.now()}.txt`);
-  console.log(tempFilePath);
+  const fileName = `${Date.now()}.txt`;
+  console.log("fileName", fileName);
+  const tempFilePath = path.join(tempDir, fileName);
 
   // Write the content to the temporary file
   await writeFileAsync(tempFilePath, content);
@@ -622,19 +109,56 @@ async function upload() {
   }
 }
 
-async function read() {
+async function uploadToNormalDirectory() {
+  const privateKey = process.env.PRIVATE_KEY;
+  const rpc = "https://gateway.tenderly.co/public/sepolia";
+  // const ethStorage = new EthStorage(rpc, privateKey, deployedSepoliaDirectory);
+  const content = "hello world!!";
+  const contentBytes = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(content));
+
+  // Create a temporary file path
+  const fileName = `${Date.now()}.txt`;
+  console.log("fileName", fileName);
+  const fileNameBytes = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(fileName));
+
+  // const provider = new ethers.providers.JsonRpcProvider(process.env.RPC);
+  const provider = new ethers.providers.JsonRpcProvider(rpc);
+  const wallet = new ethers.Wallet(privateKey, provider);
+  // const contract = new ethers.Contract(deployedSepoliaDirectory, IERC5018Abi, provider);
+  const contract = new ethers.Contract(deployedSepoliaNormalDirectory, IERC5018Abi, wallet);
+  const result = await contract.write(fileNameBytes, contentBytes);
+  console.log(result.hash);
+  await result.wait();
+  console.log("confirmed");
+}
+
+async function readFromBlobDirectory() {
   // const provider = new ethers.providers.JsonRpcProvider(process.env.RPC);
   const provider = new ethers.providers.JsonRpcProvider("http://65.108.236.27:9540");
   // const contract = new ethers.Contract(deployedSepoliaDirectory, IERC5018Abi, provider);
-  const contract = new ethers.Contract(deployedSepoliaDirectory, IERC5018Abi, provider);
-  console.log(ethers.utils.hexlify(ethers.utils.toUtf8Bytes(createdFileName)));
-  const data = await contract.read(ethers.utils.hexlify(ethers.utils.toUtf8Bytes(createdFileName)));
-  console.log(data);
-  // const hex = ethers.utils.hexlify(data);
-  // const str = ethers.utils.toUtf8String(data);
-  // console.log(str);
+  const contract = new ethers.Contract(deployedSepoliaBlobDirectory, IERC5018Abi, provider);
+  const [data] = await contract.read(ethers.utils.hexlify(ethers.utils.toUtf8Bytes(createdBlobFileName)));
+  // console.log(data);
+  const hex = ethers.utils.hexlify(data);
+  console.log(hex);
+  const str = ethers.utils.toUtf8String(data);
+  console.log("result", str);
 }
 
+async function readFromNormalDirectory() {
+  const rpc = "https://gateway.tenderly.co/public/sepolia";
+  const provider = new ethers.providers.JsonRpcProvider(rpc);
+  const contract = new ethers.Contract(deployedSepoliaNormalDirectory, IERC5018Abi, provider);
+  const [data] = await contract.read(ethers.utils.hexlify(ethers.utils.toUtf8Bytes(createdNormalFileName)));
+  const hex = ethers.utils.hexlify(data);
+  console.log(hex);
+  const str = ethers.utils.toUtf8String(data);
+  console.log("result", str);
+}
+
+// setupBlobDirectory();
 // setupNormalDirectory();
-read();
-// upload();
+// uploadToBlobDirectory();
+// uploadToNormalDirectory();
+// readFromBlobDirectory();
+// readFromNormalDirectory();
