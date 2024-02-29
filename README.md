afbd42083753a6788ce21b43cc80a8929483b008
taijusanagi <taijusanagi@gmail.com>
2024-02-29 09:04:09 -0700

    Initialize subgraph

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ https://sepolia.blobscan.com/
 ## The Graph Integration
 
 https://api.studio.thegraph.com/proxy/66919/content-nft-2/v0.0.1/graphql
+
+## Development
+
+### When contract upgrade
+
+- update the graph
+- update consumer
+- update app/src/lib/contract/index.ts addresses
--- app/src/lib/contracts/index.ts
+++ app/src/lib/contracts/index.ts
@@ -3,10 +3,10 @@ export { contentNFTAbi } from "./abi/contentNFT";
 export { storyBranchMinterL1Abi } from "./abi/storyBranchMinterL1";
 
 export const deployedSepoliaBlobDirectory = "0x53E2e6379a5697f09C8Eedd4fE05Da4f9A977269";
-export const contentNFTAddress = "0x0aaaae18c003988efa19dc6026a24cb9db2b7eb8";
-export const storyBranchMinterL1Address = "0x7fbc883e1648184ed7377a837549c6cd6d3c722a";
 
 export const sepoliaRPC = "https://rpc.sepolia.ethpandaops.io";
 export const sepoliaEthereumStorageNodeRPC = "http://65.108.236.27:9540";
 
-export const graphQLUri = "https://api.studio.thegraph.com/query/66919/storyteller/v0.0.3";
+export const contentNFTAddress = "0x84759a670722fc5666f2baff12d5db1542e47a84";
+export const storyBranchMinterL1Address = "0xc7aa2dd612ea702148031ba0afb8777e0b8ef044";
+export const graphQLUri = "https://api.studio.thegraph.com/query/66919/storyteller/v0.0.4";
