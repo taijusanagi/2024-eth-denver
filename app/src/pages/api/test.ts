36fbea96a2a8ca81e9d85797228a20cf81a92e0e
taijusanagi <taijusanagi@gmail.com>
2024-02-29 10:16:24 -0700

    simple ai 3

--- app/src/pages/api/test.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  return res.json({ content: "Hello!" });
-}
