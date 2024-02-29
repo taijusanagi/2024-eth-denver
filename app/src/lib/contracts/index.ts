c2a5c61578b156fac0a744091d2691a4ae036fdf
taijusanagi <taijusanagi@gmail.com>
2024-02-29 16:25:25 -0700

    integrate new token

--- app/src/lib/contracts/index.ts
+++ app/src/lib/contracts/index.ts
@@ -9,7 +9,7 @@ export const sepoliaEthereumStorageNodeRPC = "http://65.108.236.27:9540";
 
 export const contentNFTAddress = "0x3a5ce71db7ffe597406ee314e9995b7cbceb799c";
 export const storyBranchMinterL1Address = "0x1638ecf0f785e98aaba7864335f2cbc263b0f7ce";
-export const graphQLUri = "https://api.studio.thegraph.com/query/66919/storyteller/v0.0.9";
+export const graphQLUri = "https://api.studio.thegraph.com/query/66919/storyteller/v0.0.10";
 
 // abi path
 // ./contracts/artifacts/contracts/ContentNFT.sol/ContentNFT.json
