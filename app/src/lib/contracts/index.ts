baf72da12110c601a2c6836905575213c2554ae7
taijusanagi <taijusanagi@gmail.com>
2024-02-29 18:03:05 -0700

    Initialize subgraph

--- app/src/lib/contracts/index.ts
+++ app/src/lib/contracts/index.ts
@@ -7,8 +7,8 @@ export const deployedSepoliaBlobDirectory = "0x53E2e6379a5697f09C8Eedd4fE05Da4f9
 export const sepoliaRPC = "https://rpc.sepolia.ethpandaops.io";
 export const sepoliaEthereumStorageNodeRPC = "http://65.108.236.27:9540";
 
-export const contentNFTAddress = "0x3a5ce71db7ffe597406ee314e9995b7cbceb799c";
-export const storyBranchMinterL1Address = "0x1638ecf0f785e98aaba7864335f2cbc263b0f7ce";
+export const contentNFTAddress = "0xab3bb6f9623bf7dbe91715018c229be7df8f838b";
+export const storyBranchMinterL1Address = "0x5990a2c7ac2c7631e24d708a8cdfca047b88ff1c";
 export const graphQLUri = "https://api.studio.thegraph.com/query/66919/storyteller/v0.0.10";
 
 // abi path
