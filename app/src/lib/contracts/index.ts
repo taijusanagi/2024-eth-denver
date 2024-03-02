42da60cd6b9979d478cb91440d9bf2ec17676bb4
taijusanagi <taijusanagi@gmail.com>
2024-03-01 19:35:20 -0700

    update contract

--- app/src/lib/contracts/index.ts
+++ app/src/lib/contracts/index.ts
@@ -7,9 +7,9 @@ export const deployedSepoliaBlobDirectory = "0x53E2e6379a5697f09C8Eedd4fE05Da4f9
 export const sepoliaRPC = "https://rpc.sepolia.ethpandaops.io";
 export const sepoliaEthereumStorageNodeRPC = "http://65.108.236.27:9540";
 
-export const contentNFTAddress = "0xb935d4bdd0c0a5e5f0f84ed27cdb9565e7bc62d9";
-export const storyBranchMinterL1Address = "0xd876396cce9b05e275f8da8f030830291b066db3";
-export const graphQLUri = "https://api.studio.thegraph.com/query/66919/storyteller/v0.0.14";
+export const contentNFTAddress = "0x4c7d9dc0e7343cb4e6045cd9d3245ac6d091b1ae";
+export const storyBranchMinterL1Address = "0x59961463a624e48120850f1937ba5c7ca3ef16fc";
+export const graphQLUri = "https://api.studio.thegraph.com/query/66919/storyteller/v0.0.15";
 
 // abi path for the graph build
 // ./contracts/artifacts/contracts/ContentNFT.sol/ContentNFT.json
